73b9ad340178d2bb67011e9c965ccc100b7e2b2e
huyong1109 <huyong1109@gmail.com>
2014-10-30 15:58:20 -0600

    concise version

---
+++ run0.sh
@@ -0,0 +1,15 @@
+#!/bin/tcsh
+#PBS -N MPI_TEST
+#PBS -q long
+#PBS -l nodes=1:ppn=8
+#PBS -r n
+#PBS -j oe
+#PBS -S /bin/csh -V
+#cd ${PBS_O_WORKDIR}
+source ~/env_machopts.frankfurt_intel
+setenv CASE 0
+$MPICH_PATH/bin/mpif77 prep$CASE.f -L/usr/local/intel-cluster/mkl/lib/intel64/ -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lmkl_lapack95_lp64 -lmkl_blacs_lp64 -o prep$CASE
+$MPICH_PATH/bin/mpirun -np 4 ./prep$CASE #>output_prep$CASE
+$MPICH_PATH/bin/mpif77 marching$CASE.f -L/usr/local/intel-cluster/mkl/lib/intel64/ -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lmkl_lapack95_lp64 -lmkl_blacs_lp64 -o marching$CASE
+$MPICH_PATH/bin/mpirun -np 4 ./marching$CASE #>output_marching$CASE
+
